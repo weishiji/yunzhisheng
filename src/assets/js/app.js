2ef4c2f87712ba03c94976a246fcf415f26bbbeb
lxg <lxg1986@gmail.com>
2016-10-16 14:57:53 +0800

    增加好多切图

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -112,13 +112,16 @@ particlesJS("particles-js", {
 var $banner = $('div[data-banner]');
 if($banner.length !== 0){
   $topMenu = $('#main-menu');
+  $logo = $topMenu.find('.title-area img')
   function scrollY(){
     var scrollBarPos = $(this).scrollTop();
     var bannerHeight = $banner.outerHeight(true);
     if(scrollBarPos > bannerHeight){
       $topMenu.addClass('fixed-color');
+      $logo.attr('src','/assets/img/logo_blue.png');
     }else{
       $topMenu.removeClass('fixed-color');
+      $logo.attr('src','/assets/img/logo_white.png');
     }
   };
   scrollY();
