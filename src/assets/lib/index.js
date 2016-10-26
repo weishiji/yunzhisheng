6c68bc0813a5fad481eb2b2ace694422a0203f35
lxg <lxg1986@gmail.com>
2016-10-26 23:41:07 +0800

    首页增加一个弹出层，为视频做准备

--- src/assets/lib/index.js
+++ src/assets/lib/index.js
@@ -706,17 +706,17 @@ particlesJS("particles-js-2", {
   },
   "retina_detect": true
 });
+//TODO:视频播放
+// var player = videojs('really-cool-video', { /* Options */ }, function() {
+//   console.log('Good to go!');
 
-var player = videojs('really-cool-video', { /* Options */ }, function() {
-  console.log('Good to go!');
+//   this.play(); // if you don't trust autoplay for some reason
 
-  this.play(); // if you don't trust autoplay for some reason
-
-  // How about an event listener?
-  this.on('ended', function() {
-    console.log('awww...over so soon?');
-  });
-});
+//   // How about an event listener?
+//   this.on('ended', function() {
+//     console.log('awww...over so soon?');
+//   });
+// });
 
 
 
